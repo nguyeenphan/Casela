cf07fdbec431945e632edbbfd750668e274977c1
nguyeenphan <phannguyenbh0902@gmail.com>
2024-12-11 11:32:53 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,163 @@
-import Image from "next/image";
-
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
+import { Check, CheckCheck, Star } from "lucide-react";
+import { Icons } from "@/components/Icon";
+import { Reviews } from "@/components/Reviews";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 
+        sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center 
+            lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-36 left-0 -top-20 hidden lg:block">
+                <img src="/icon-1.png" alt="pic" className="w-full"/>
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 
+              font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Hô biến ảnh của bạn lên <span className="bg-amber-500 px-2 py-2 text-white rounded-[0.5rem]">Ốp lưng</span> Độc đáo
+                </h1>
+                <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                  Lưu giữ kỷ niệm đẹp với ốp lưng <span
+                  className="font-semibold">độc nhất</span> của riêng bạn.
+                  Casela giúp bạn bảo vệ không chỉ chiếc điện thoại mà cả những ký ức quý giá.
+                </p>
+                <ul className="mt-8 space-y-2 text-left font-medium flex
+                flex-col items-center sm:items-start">
+                  <div className="space-y-2">
+                    <li className="flex gap-1.5 items-center text-left">
+                      <CheckCheck className="h-5 w-5 shrink-0 text-amber-500" strokeWidth={3}/>
+                      Chất liệu cao cấp, bền bỉ
+                    </li>
+                    <li className="flex gap-1.5 items-center text-left">
+                      <CheckCheck className="h-5 w-5 shrink-0 text-amber-500" strokeWidth={3}/>
+                      Bảo hành in ấn 5 năm
+                    </li>
+                    <li className="flex gap-1.5 items-center text-left">
+                      <CheckCheck className="h-5 w-5 shrink-0 text-amber-500" strokeWidth={3}/>
+                      Hỗ trợ các mẫu iPhone hiện đại
+                    </li>
+                  </div>
+                </ul>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <div className="mt-12 flex flex-col sm:flex-row items-center
+                sm:items-start gap-5">
+                  <div className="flex -space-x-4">
+                    <img className="inlince-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user"/>
+                    <img className="inlince-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user"/>
+                    <img className="inlince-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user"/>
+                    <img className="inlince-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-4.png"
+                    alt="user"/>
+                    <img className="inlince-block h-10 w-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-5.png"
+                    alt="user"/>
+                  </div>
+
+                  <div className="flex flex-col justify-between items-center sm:items-start">
+                    <div className="flex gap=0.5">
+                      <Star className="h-4 w-4 text-amber-500 fill-amber-500"/>
+                      <Star className="h-4 w-4 text-amber-500 fill-amber-500"/>
+                      <Star className="h-4 w-4 text-amber-500 fill-amber-500"/>
+                      <Star className="h-4 w-4 text-amber-500 fill-amber-500"/>
+                      <Star className="h-4 w-4 text-amber-500 fill-amber-500"/>
+                    </div>
+                    <p>
+                      <span className="font-semibold">8386</span> khách hàng hài lòng
+                    </p>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img src="/your-image.png" alt="" className="absolute w-40 lg:w-52 left-56 -top-20 seidden smlect-none h:block lg:hidden xl:block" />
+              <img src="/line.png" alt="" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <Phone className= "w-64 rounded-[2.5rem]" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* value proposition section */}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+              <span className="relative px-2">Khách hàng 
+                <Icons.underline className="hidden sm:block pointer-events-none 
+                absolute inset-x-0 -bottom-6 text-amber-500"/></span>nghĩ gì về Casela
+            </h2>
+            {/* <img src="/snake-2.png" className="w-24 order-0 lg:order-2" alt="" /> */}
+          </div>
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 
+          lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                &quot;Ốp lưng đẹp xuất sắc, <span className="p-0.5 bg-slate-800 text-white rounded">chất lượng in ấn tốt, bền bỉ</span>, giá cả phải chăng. Mình đã mua nhiều lần và rất hài lòng với chất lượng sản phẩm. Đặc biệt là dịch vụ chăm sóc khách hàng rất tốt.&quot;
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img src="/users/user-1.png" className="rounded-full h-12 w-12 object-cover" alt="" />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Quynh Anh</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-amber-500"/>
+                    <p className="text-sm">Đã mua hàng</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* 2nd review */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              <Star className="h-5 w-5 text-amber-500 fill-amber-500"/>
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                &quot;Shop phục vụ rất <span className="p-0.5 bg-slate-800 text-white rounded">tận tình, tư vấn nhiệt tình</span>. Ốp lưng đẹp y hình, giao hàng nhanh.&quot;
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img src="/users/user-5.png" className="rounded-full h-12 w-12 object-cover" alt="" />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Quang Khai</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 stroke-[3px] text-amber-500"/>
+                    <p className="text-sm">Đã mua hàng</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+        {/* <Reviews/> */}
+        <div className="pt-16">
+          
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
   );
 }
